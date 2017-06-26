49be238f71bff96504b64dce0a2552ab03e536b7
Joseph Carlson <guitarjoe96@yahoo.com>
2017-06-26 16:25:43 -0400

    Handle Clicks on the Button and the instructions paragraph

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-console.log("it worked");
 
-function changedHeading(){
-const heading = document.querySelector('h1')
-heading.textContent = 'Ye Olde Person Stats';
+
+function changedHeading(ev){
+    debugger
+
+p.textContent = 'Clickety Click'
 }
 
 const btn = document.querySelector('button')
 btn.addEventListener('click', changedHeading)
+
+const p = document.querySelector('#instructions')
+p.addEventListener('click', changedHeading)
