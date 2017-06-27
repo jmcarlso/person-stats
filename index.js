87984ccf24216c8bc7abfb26de577658443789e0
Joseph Carlson <guitarjoe96@yahoo.com>
2017-06-26 20:46:45 -0400

    Homework 1 Change -adding stat line and changing it to the input from the form

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ function changedHeading(ev){
     const heading = document.querySelector('h1')
     const f = ev.target
     const name = f.personName.value
-heading.textContent = name
+    heading.textContent = name
+
+    const stat = document.querySelector('#hw')
+    const statChange = f.personName.value
+    stat.textContent = name
 }
 
 const personForm = document.querySelector('#person-form')
