9d62d98cecea75749bfcd67059a1743bc9188287
Joseph Carlson <guitarjoe96@yahoo.com>
2017-06-27 16:03:58 -0400

    added result as list

--- index.js
+++ index.js
@@ -7,11 +7,32 @@ function changedHeading(ev){
     const name = f.personName.value
     const stat = document.querySelector('#hw')
     const statChange = f.personAge.value
-    
-    stat.textContent = name +': ' + statChange
-
     var color = document.getElementById("color").value;
-    stat.style.color = color;
+   // stat.textContent = name +': ' + statChange
+const list = document.createElement('ul')
+
+const nameItem = document.createElement('li')
+nameItem.textContent = `Name: ${name}`
+const ageItem =document.createElement('li')
+ageItem.textContent =  `Age: ${statChange}`
+
+const colorItem = document.createElement('li')
+colorItem.textContent = `Favorite Color: ${color}`
+colorItem.style.backgroundColor = color
+
+const colorDiv = document.createElement('div')
+colorDiv.style.backgroundColor = color
+colorItem.appendChild(colorDiv)
+colorDiv.style.width = '6rem'
+colorDiv.style.height = '3rem'
+
+list.appendChild(nameItem)
+list.appendChild(ageItem)
+list.appendChild(colorItem)
+stats.appendChild(list)
+
+    
+    stats.style.color = color;
 }
 
 const personForm = document.querySelector('#person-form')
