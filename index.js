d38cb2f0882c750a43ed31ba593410b0fdfc7f28
Joseph Carlson <guitarjoe96@yahoo.com>
2017-06-26 20:54:13 -0400

    Homework 1a revised

--- index.js
+++ index.js
@@ -2,11 +2,9 @@
 
 function changedHeading(ev){
     ev.preventDefault()
-    const heading = document.querySelector('h1')
+    
     const f = ev.target
     const name = f.personName.value
-    heading.textContent = name
-
     const stat = document.querySelector('#hw')
     const statChange = f.personName.value
     stat.textContent = name
