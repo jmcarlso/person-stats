a0f6cd6814da739666c3181a743e97c2f8d90dd1
Joseph Carlson <guitarjoe96@yahoo.com>
2017-06-28 00:44:01 -0400

    created functions to create list and list items

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-
-function renderColor(color){
-
-const colorDiv = document.createElement('div')
-colorDiv.style.backgroundColor = color
-colorDiv.style.width = '6rem'
-colorDiv.style.height = '3rem'
-
-return colorDiv
-}
 function changedHeading(ev){
     ev.preventDefault()
     
@@ -17,28 +7,54 @@ function changedHeading(ev){
     const statChange = f.personAge.value
     var color = document.getElementById("color").value;
    // stat.textContent = name +': ' + statChange
-const list = document.createElement('ul')
+//const list = document.createElement('ul')
 
-const nameItem = document.createElement('li')
-nameItem.textContent = `Name: ${name}`
-const ageItem =document.createElement('li')
-ageItem.textContent =  `Age: ${statChange}`
-
-const colorItem = document.createElement('li')
-colorItem.textContent = `Favorite Color: `
-//colorItem.style.backgroundColor = color
-
-
-colorItem.appendChild(renderColor(color))
-
-list.appendChild(nameItem)
-list.appendChild(ageItem)
-list.appendChild(colorItem)
-stats.appendChild(list)
+//const nameItem = document.createElement('li')
+renderList(name,statChange,color)
+//list.appendChild(ageItem)
+//list.appendChild(colorItem)
+stats.appendChild(renderList(name,statChange,color))
 
     
     stats.style.color = color;
 }
+function renderColor(color){
+
+const colorDiv = document.createElement('div')
+colorDiv.style.backgroundColor = color
+colorDiv.style.width = '6rem'
+colorDiv.style.height = '3rem'
+
+return colorDiv
+}
+
+function renderListItem(){
+
+const item = document.createElement('li')
+return item
+}
+
+function renderList(name,statChange,color){
+const list = document.createElement('ul')
+const item = []
+
+for(var i = 0; i<3;i++){
+item[i] = renderListItem()
+}
+item[0].textContent = `Name: ${name}`
+//const ageItem =document.createElement('li')
+item[1].textContent =  `Age: ${statChange}`
+
+//const colorItem = document.createElement('li')
+item[2].textContent = `Favorite Color: `
+//item[2].style.backgroundColor = color
+item[2].appendChild(renderColor(color))
+for(var j = 0;j< item.length;j++){
+    list.appendChild(item[j])
+}
+return list
+}
+
 
 const personForm = document.querySelector('#person-form')
 personForm.addEventListener('submit', changedHeading)
